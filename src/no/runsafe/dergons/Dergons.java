2f16b2814eb4b025fed0cbcdb89f198b8cd336a6
Kruithne <kruithne@gmail.com>
2014-02-05 03:06:25 +0000

    Initialized new plugin Dergons from template

---
+++ src/no/runsafe/dergons/Dergons.java
@@ -0,0 +1,26 @@
+package no.runsafe.dergons;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class Dergons extends RunsafePlugin
+//public class Dergons extends RunsafeConfigurablePlugin
+{
+//	public static IDebug Debugger = null;
+
+	@Override
+	protected void pluginSetup()
+	{
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(LUAScripts.class);
+//		addComponent(UniverseRegistration.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
+	}
+}
