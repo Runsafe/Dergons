847127b5edd3309c1a08a3088075089a2b6b80ca
Kruithne <kruithne@gmail.com>
2014-02-12 09:36:23 +0000

    Revert last change, does not work as intended.

--- src/no/runsafe/dergons/EntityMonitor.java
+++ src/no/runsafe/dergons/EntityMonitor.java
@@ -1,12 +1,9 @@
 package no.runsafe.dergons;
 
-import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.event.entity.IEntityDamageByEntityEvent;
-import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.entity.LivingEntity;
 import no.runsafe.framework.minecraft.entity.ProjectileEntity;
 import no.runsafe.framework.minecraft.entity.RunsafeEntity;
-import no.runsafe.framework.minecraft.entity.RunsafeProjectile;
 import no.runsafe.framework.minecraft.event.entity.RunsafeEntityDamageByEntityEvent;
 
 public class EntityMonitor implements IEntityDamageByEntityEvent
@@ -25,23 +22,11 @@ public class EntityMonitor implements IEntityDamageByEntityEvent
 			RunsafeEntity attacker = event.getDamageActor();
 			if (attacker.getEntityType() == ProjectileEntity.Arrow)
 			{
-				RunsafeProjectile arrow = (RunsafeProjectile) attacker;
 				for (Dergon dergon : handler.getDergons())
 				{
 					if (dergon.isDergon(entity))
 					{
-						IPlayer shooter = arrow.getShooterPlayer();
-						if (shooter != null)
-						{
-							ILocation playerLocation = shooter.getLocation();
-							ILocation arrowLocation = arrow.getLocation();
-							if (playerLocation != null && arrowLocation != null)
-								arrow.setVelocity(playerLocation.toVector().subtract(arrowLocation.toVector()).normalize());
-						}
-						else
-						{
-							arrow.remove();
-						}
+						attacker.remove();
 						dergon.getDragon().damage(5.0D);
 						event.cancel();
 					}
