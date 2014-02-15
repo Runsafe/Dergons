7e88c2f6da837c0de6ab0d87ae4307a003427049
Kruithne <kruithne@gmail.com>
2014-02-15 00:09:23 +0000

    We already add the entity to the world, no need to do that.

--- src/no/runsafe/dergons/Dergon.java
+++ src/no/runsafe/dergons/Dergon.java
@@ -73,7 +73,6 @@ public class Dergon
 					livingDragon.setMaxHealth(health);
 					livingDragon.setHealth(health);
 				}
-				rawWorld.addEntity(dragon);
 			}
 
 			fireballTimer = scheduler.startSyncRepeatingTask(new Runnable()
