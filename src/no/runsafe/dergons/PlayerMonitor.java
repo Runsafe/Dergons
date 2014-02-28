fa533de3bd4b8af44da7ecf759f74e5e19f31897
Kruithne <kruithne@gmail.com>
2014-02-28 02:26:10 +0000

    Use physics rather than block break, dragon eggs are never broken.

--- src/no/runsafe/dergons/PlayerMonitor.java
+++ src/no/runsafe/dergons/PlayerMonitor.java
@@ -2,21 +2,21 @@ package no.runsafe.dergons;
 
 import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.block.IBlock;
-import no.runsafe.framework.api.event.block.IBlockBreak;
-import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.api.event.block.IBlockPhysics;
 import no.runsafe.framework.minecraft.Item;
+import no.runsafe.framework.minecraft.event.block.RunsafeBlockPhysicsEvent;
 
-public class PlayerMonitor implements IBlockBreak
+public class PlayerMonitor implements IBlockPhysics
 {
 	@Override
-	public boolean OnBlockBreak(IPlayer player, IBlock block)
+	public void OnBlockPhysics(RunsafeBlockPhysicsEvent event)
 	{
+		IBlock block = event.getBlock();
 		if (block.is(Item.Special.DragonEgg))
 		{
 			ILocation location = block.getLocation();
 			location.getWorld().dropItem(location, DergonItems.getEgg(1));
-			return false;
+			event.cancel();
 		}
-		return true;
 	}
 }
