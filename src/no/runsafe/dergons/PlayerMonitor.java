2d8d81935e08bb0b3934f7dc62e713d15997cb4c
Kruithne <kruithne@gmail.com>
2014-02-28 02:47:42 +0000

    Use OnItemSpawn event.

--- src/no/runsafe/dergons/PlayerMonitor.java
+++ src/no/runsafe/dergons/PlayerMonitor.java
@@ -1,20 +1,18 @@
 package no.runsafe.dergons;
 
 import no.runsafe.framework.api.ILocation;
-import no.runsafe.framework.api.block.IBlock;
-import no.runsafe.framework.api.event.block.IBlockPhysics;
+import no.runsafe.framework.api.event.entity.IItemSpawn;
 import no.runsafe.framework.minecraft.Item;
-import no.runsafe.framework.minecraft.event.block.RunsafeBlockPhysicsEvent;
+import no.runsafe.framework.minecraft.event.entity.RunsafeItemSpawnEvent;
 
-public class PlayerMonitor implements IBlockPhysics
+public class PlayerMonitor implements IItemSpawn
 {
 	@Override
-	public void OnBlockPhysics(RunsafeBlockPhysicsEvent event)
+	public void OnItemSpawn(RunsafeItemSpawnEvent event)
 	{
-		IBlock block = event.getBlock();
-		if (block.is(Item.Special.DragonEgg))
+		if (event.getEntity().getItemStack().is(Item.Special.DragonEgg))
 		{
-			ILocation location = block.getLocation();
+			ILocation location = event.getLocation();
 			location.getWorld().dropItem(location, DergonItems.getEgg(1));
 			event.cancel();
 			Dergons.Debugger.debugInfo("Dropping dergon egg!");
