b84fdf5fbef4098b508b41951132ed4fd64e66da
Kruithne <kruithne@gmail.com>
2014-03-03 00:23:44 +0000

    Updated to reflect upstream API changes.

--- src/no/runsafe/dergons/commands/SpawnDergon.java
+++ src/no/runsafe/dergons/commands/SpawnDergon.java
@@ -2,7 +2,7 @@ package no.runsafe.dergons.commands;
 
 import no.runsafe.dergons.DergonHandler;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.OnlinePlayerRequired;
+import no.runsafe.framework.api.command.argument.Player;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.player.IPlayer;
 
@@ -10,14 +10,14 @@ public class SpawnDergon extends PlayerCommand
 {
 	public SpawnDergon(DergonHandler handler)
 	{
-		super("spawn", "Spawn a dergon", "runsafe.dergons.spawn", new OnlinePlayerRequired());
+		super("spawn", "Spawn a dergon", "runsafe.dergons.spawn", new Player.Online.Required());
 		this.handler = handler;
 	}
 
 	@Override
 	public String OnExecute(IPlayer executor, IArgumentList parameters)
 	{
-		IPlayer targetPlayer = parameters.getPlayer("player");
+		IPlayer targetPlayer = parameters.getValue("player");
 		if (targetPlayer != null && targetPlayer.isOnline())
 		{
 			handler.spawnDergon(targetPlayer.getLocation());
